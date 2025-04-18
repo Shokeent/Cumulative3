ca4336b0f50578434cc8a1c981821cfd3dfe48b3
Tarun Shokeen <shokeentarun20@gmail.com>
2025-04-18 17:14:44 -0400

    added cumulative part 3

--- Cumulative01/Cumulative01/obj/Debug/net8.0/Cumulative01.AssemblyInfo.cs
+++ Cumulative01/Cumulative01/obj/Debug/net8.0/Cumulative01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cumulative01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b94d9cabf204e8d1404ae410e7ded55cda2d01")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cumulative01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cumulative01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
